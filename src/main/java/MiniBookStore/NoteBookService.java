bd041a09eb8e9c87e19704040ea01fa92edb6f99
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-17 18:56:43 +0300

    MiniBook Store Project

---
+++ src/main/java/MiniBookStore/NoteBookService.java
@@ -0,0 +1,39 @@
+package MiniBookStore;
+//2-b ->>NoteBook ile ilgili işlemler
+public class NoteBookService implements ProductService{
+    @Override
+    public void processMenu() {
+
+    }
+
+    @Override
+    public void listProduct() {
+
+    }
+
+    @Override
+    public void addProduct() {
+
+    }
+
+    @Override
+    public void addStock(int miktar) {
+
+    }
+
+
+    @Override
+    public void deleteProduct() {
+
+    }
+
+    @Override
+    public void updateProduct() {
+
+    }
+
+    @Override
+    public void filterProducts(String filter) {
+
+    }
+}
---
+++ src/main/java/MiniBookStore/ProductService.java
@@ -0,0 +1,16 @@
+package MiniBookStore;
+//2- ->>product işlemleri için bir standart oluşturuyorum.
+//bu demektir ki ben ürünlerle ilgili bir servis class'i oluşturacağım zaman
+//burada yazdığım method'lari kullanmak zorundayım.
+public interface ProductService {
+
+    void processMenu();
+    void listProduct();
+    void addProduct();
+    void addStock(int miktar);
+    void deleteProduct();
+    void updateProduct();
+    void filterProducts(String filter);
+
+
+}
